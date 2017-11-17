ac5989c66205b868c8fe2c1b21b88f35831e38df
Shannon <ldsz89@mail.missouri.edu>
2017-11-16 18:24:20 -0600

    Added dataSource and delegate link for picker view

--- Color Picker/Color Picker/ColorPickerViewController.swift
+++ Color Picker/Color Picker/ColorPickerViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ColorPickerViewController: UIViewController {
+class ColorPickerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     @IBOutlet weak var colorLabel: UILabel!
     @IBOutlet weak var colorPickerView: UIPickerView!
@@ -21,8 +21,26 @@ class ColorPickerViewController: UIViewController {
                   Color(name: "Purple", uiColor: UIColor.purple),
                   Color(name: "Brown", uiColor: UIColor.brown)]
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row].name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        colorLabel.text = colors[row].name
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        colorLabel.text = colors[0].name
 
         // Do any additional setup after loading the view.
     }
