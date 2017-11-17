8628f37bd813421d476d14c1a744998b8413de4a
Shannon <ldsz89@mail.missouri.edu>
2017-11-16 18:12:28 -0600

    Added UI elements

---
+++ Color Picker/Color Picker/ColorPickerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ColorPickerViewController.swift
+//  Color Picker
+//
+//  Created by Shannon, Leandre D. (MU-Student) on 11/16/17.
+//  Copyright © 2017 Shannon, Leandre D. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
